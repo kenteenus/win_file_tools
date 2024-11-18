8a84dfc8c0446189eb662fa78840ffd0c6bdea3e
kenteenus <sumaho3go@gmail.com>
2024-11-19 07:16:08 +0900

    1st update

---
+++ Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TreeView1 = New TreeView()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        SuspendLayout()
+        ' 
+        ' TreeView1
+        ' 
+        TreeView1.BorderStyle = BorderStyle.None
+        TreeView1.Dock = DockStyle.Fill
+        TreeView1.Font = New Font("游ゴシック", 9.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(128))
+        TreeView1.Indent = 35
+        TreeView1.ItemHeight = 25
+        TreeView1.LineColor = Color.Blue
+        TreeView1.Location = New Point(0, 0)
+        TreeView1.Margin = New Padding(0)
+        TreeView1.Name = "TreeView1"
+        TreeView1.Size = New Size(464, 601)
+        TreeView1.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(296, 0)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(35, 25)
+        Button1.TabIndex = 1
+        Button1.Text = "展+"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(334, 0)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(35, 25)
+        Button2.TabIndex = 2
+        Button2.Text = "閉-"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = SystemColors.ControlDark
+        Button3.Location = New Point(372, 0)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(38, 25)
+        Button3.TabIndex = 3
+        Button3.Text = "透け"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.DarkGray
+        Button4.Location = New Point(413, 0)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(38, 25)
+        Button4.TabIndex = 4
+        Button4.Text = "Top"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AcceptButton = Button1
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        CancelButton = Button2
+        ClientSize = New Size(464, 601)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TreeView1)
+        Name = "Form1"
+        Text = "Treeeen"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents TreeView1 As TreeView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,68 @@
+﻿Public Class Form1
+    Dim stat As Boolean = False
+    Dim opa As Single = 1.0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim fbd As New FolderBrowserDialog
+        fbd.Description = "select Fordacnot short cut>"
+        fbd.RootFolder = Environment.SpecialFolder.Desktop
+        fbd.SelectedPath = "C:\Windows"
+        fbd.ShowNewFolderButton = True
+        If fbd.ShowDialog(Me) = DialogResult.OK Then
+            Console.WriteLine(fbd.SelectedPath)
+        End If
+        trViewSet(fbd.SelectedPath, TreeView1.Nodes.Add(fbd.SelectedPath))
+
+    End Sub
+
+    Private Sub trViewSet(ByVal path As String, ByVal node As TreeNode)
+        Dim folder As System.IO.DirectoryInfo = New System.IO.DirectoryInfo(path)
+        Dim subfolders() As System.IO.DirectoryInfo = folder.GetDirectories()
+        Dim subfolder As System.IO.DirectoryInfo
+        For Each subfolder In subfolders
+            Dim subnode As TreeNode = node.Nodes.Add(subfolder.Name)
+            trViewSet(subfolder.FullName, subnode)
+
+        Next
+        Dim files() As System.IO.FileInfo = folder.GetFiles()
+        Dim file As System.IO.FileInfo
+        For Each file In files
+            node.Nodes.Add(file.Name)
+        Next
+    End Sub
+    Private Sub TreeView1_AfterSelect(ByVal sender As Object, ByVal e As System.Windows.Forms.TreeViewEventArgs) Handles TreeView1.AfterSelect
+        Dim result As DialogResult = MessageBox.Show(e.Node.FullPath & vbCrLf & "copy?", "ok", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If result = DialogResult.Yes Then
+            Clipboard.SetText(e.Node.FullPath)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TreeView1.ExpandAll()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TreeView1.CollapseAll()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        stat = Not stat
+        Me.TopMost = stat
+        If stat = True Then
+            Button4.BackColor = Color.Aqua
+        Else
+            Button4.BackColor = Color.DarkGray
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        opa = opa - 0.1
+        If opa < 0.5 Then
+            opa = 1.0
+        End If
+
+        Me.Opacity = opa
+    End Sub
+End Class
+
+
